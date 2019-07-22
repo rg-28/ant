cc0dcbed2dad2c92684701df18250f8892f84cf7
rg-28 <rajang597@gmail.com>
2019-07-22 21:29:42 +0530

    Modified the odometry node

--- src/odometry_node.cpp
+++ src/odometry_node.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "odometry_node");
 
   ros::NodeHandle n;
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("imu_link", 50);
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odometry_node", 50);
   tf::TransformBroadcaster odom_broadcaster;
 
   double x = 0.0;
